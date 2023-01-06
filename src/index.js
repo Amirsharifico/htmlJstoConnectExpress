20381f735ae7aef3607dac58792b9471768309a8
amirsharifico <amirsharifico@gmail.com>
2023-01-06 14:44:34 -0500

    fetch data from express

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let url = "http://localhost:3000/";
+
+function myFirstFunction() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("javab").innerText = data[0].name;
+    });
+}
